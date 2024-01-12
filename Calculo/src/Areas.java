5f95f3113973fe50f14029fad85a497d0ed0574d
Mario García <mario.meca2005@gmail.com>
2024-01-12 17:00:04 +0100

    añadido área pentágono, revisar área cuadrado(necesita correción)

--- Calculo/src/Areas.java
+++ Calculo/src/Areas.java
@@ -9,10 +9,19 @@ public class Areas {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(areaCuadrado(sc.nextInt()));
+//        System.out.println(areaCuadrado(sc.nextInt()));
+        System.out.print("Perímetro: ");
+        double p = Double.parseDouble(sc.nextLine());
+        System.out.print("Apotema: ");
+        double a = Double.parseDouble(sc.nextLine());
+        System.out.println(areaPentagono(p,a));
     }
 
     public static double areaCuadrado(double a) {
         return Math.pow(a, a);
+    }
+    
+    public static double areaPentagono(double p, double a){
+        return (p*a)/2;
     }
 }
