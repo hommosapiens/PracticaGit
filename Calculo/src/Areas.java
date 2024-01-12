96126c4cfa3684fe70818f617ecd8ed0a18a8444
Pedro <pedrotrabajos22@gmail.com>
2024-01-12 17:13:50 +0100

    Arreglado problema areaCuadrado, por favor haz limpieza de código

--- Calculo/src/Areas.java
+++ Calculo/src/Areas.java
@@ -9,19 +9,22 @@ public class Areas {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//        System.out.println(areaCuadrado(sc.nextInt()));
+        System.out.println(areaCuadrado(sc));
         System.out.print("Perímetro: ");
         double p = Double.parseDouble(sc.nextLine());
         System.out.print("Apotema: ");
         double a = Double.parseDouble(sc.nextLine());
-        System.out.println(areaPentagono(p,a));
+        System.out.println(areaPentagono(p, a));
     }
 
-    public static double areaCuadrado(double a) {
+    public static double areaCuadrado(Scanner sc) {
+        System.out.println("Introduzca la longitud del lado");
+        Double a = Double.parseDouble(sc.nextLine());
         return Math.pow(a, a);
     }
-    
-    public static double areaPentagono(double p, double a){
-        return (p*a)/2;
+
+    public static double areaPentagono(double p, double a) {
+
+        return (p * a) / 2;
     }
 }
