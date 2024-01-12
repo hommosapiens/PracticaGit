af20d15ccbc4cc7ca4447e400a6e5f78a2be99dd
Pedro <pedrotrabajos22@gmail.com>
2024-01-12 15:47:02 +0100

    Area cuadrado añadida

--- Calculo/src/Areas.java
+++ Calculo/src/Areas.java
@@ -1,10 +1,18 @@
 
+import java.util.Scanner;
+
 /**
  *
  * @author Juan Pedro.
  */
 public class Areas {
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println(areaCuadrado(sc.nextInt()));
+    }
+
+    public static double areaCuadrado(double a) {
+        return Math.pow(a, a);
     }
 }
